952f23766ddea9fee9ce3084f4617c4197900923
Adrian Grabkowski <adrian.grabkowski@wp.pl>
2020-09-03 01:09:17 +0200

    fetching data from API

--- src/pages/ships.js
+++ src/pages/ships.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
 class Ships extends React.Component {
+    state = {
+        ships: []
+    }
+
+    componentDidMount() {
+        fetch('https://swapi.apis.guru/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                query: `
+                    query {
+                        allStarships {
+                        starships {
+                            name
+                            manufacturers
+                            costInCredits
+                        }
+                        }
+                    }
+                `
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            this.setState({ ships : data.data.allStarships.starships });
+        });
+    }
+
     render () {
+        console.log(this.state.ships);
         return (
             <div>Ships List</div>
         )
